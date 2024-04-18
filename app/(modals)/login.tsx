0532f71fb23f6b8dddf7725f64bea2bed2bf2f4a
josef-sho <josephshodunke4@gmail.com>
2024-04-18 14:17:24 +0100

    updating styles

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
+import { useWarmUpBrowser } from '../hooks/usewarmupbrowser'
 
 const login = () => {
+  useWarmUpBrowser
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <TextInput placeholder="Email"  autoCapitalize='none'/>   
+      <TextInput placeholder="Password" secureTextEntry={true} autoCapitalize='none'/>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'#fff',
+      padding:26
+    }
+})
 export default login
---
+++ app/hooks/usewarmupbrowser.ts
@@ -0,0 +1,12 @@
+// use warm up browser . ts
+import { useEffect } from 'react';
+
+import * as WebBrowser from 'expo-web-browser';
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    void WebBrowser.warmUpAsync();
+  return () => {
+    void WebBrowser.coolDownAsync();
+  } }, []);
+}
