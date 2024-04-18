49599cf1f85d5524b4299b89b0d36b4d15192689
josef-sho <josephshodunke4@gmail.com>
2024-04-18 15:53:00 +0100

    few changes

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,22 +1,65 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import React from 'react'
-import { useWarmUpBrowser } from '../hooks/usewarmupbrowser'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useWarmUpBrowser } from "../hooks/usewarmupbrowser";
+import { defaultStyles } from "@/constants/styles";
+import Colors from "@/constants/Colors";
 
 const login = () => {
-  useWarmUpBrowser
+  useWarmUpBrowser;
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Email"  autoCapitalize='none'/>   
-      <TextInput placeholder="Password" secureTextEntry={true} autoCapitalize='none'/>
+      <TextInput
+        placeholder="Email"
+        autoCapitalize="none"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry={true}
+        autoCapitalize="none"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={[defaultStyles.btn]}>
+        <Text style={defaultStyles.btnText}> Continue </Text>
+      </TouchableOpacity> 
+      <View style={styles.seperatorview}>
+        <View 
+        style={{ 
+        flex:1,
+        borderBottomWidth: StyleSheet.hairlineWidth, 
+        borderBottomColor: "black" }} 
+        />
+        <Text style={styles.seperator}>Or</Text>
+        <View 
+        style={{ 
+        flex:1,
+        borderBottomWidth: StyleSheet.hairlineWidth, 
+        borderBottomColor: "black" }} 
+        />
+        
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor:'#fff',
-      padding:26
-    }
-})
-export default login
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 26,
+  },
+  seperatorview:{
+    flexDirection: 'row',
+  },
+  seperator: {
+    fontFamily: 'SpaceMono',
+    color: Colors.grey,
+  }
+});
+export default login;
