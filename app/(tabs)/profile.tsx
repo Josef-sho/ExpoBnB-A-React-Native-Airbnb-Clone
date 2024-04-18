4899f3654b2e97ef5fb7ace239395b36422d48fc
josef-sho <josephshodunke4@gmail.com>
2024-04-18 11:52:51 +0100

    changing profile page and adding clerk

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
+import { Link } from '@react-navigation/native'
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth()
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title='Logout' onPress={() => signOut()}/>
+     {!isSignedIn && ( <Link to={'/(modals)/login'}><Text>Login</Text></Link> )}
     </View>
   )
 }
